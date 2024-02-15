6dd1c82618ec95e3576118f5635101d64a1e5e3c
ZertakSapr <sepreanton@gmail.com>
2024-02-15 22:24:31 +0300

    Last

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass[] mass = new BaseClass[3];
+            Console.WriteLine("Как вы хотите заполянть массив одномерных");
+            bool flagone = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Какое кол-во злементов вы хотите в одномерном масииве");
+            int count = int.Parse(Console.ReadLine());
+            if (flagone)
+            {
+                mass[0] = new OneDimension( count, flagone);
+            }
+            else
+            {
+                mass[0] = new OneDimension( count);
+            }
+           
+
+            Console.WriteLine("Как вы хотите заполянть массив двумерных");
+            bool flagtwo = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Какое кол-во строк вы хотите в двумерном масииве");
+            int counttwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Какое кол-во столбцов вы хотите в двумерном масииве");
+            int counttwoс = int.Parse(Console.ReadLine());
+            
+            if (flagtwo)
+            {
+                mass[1] = new TwoDimensions( counttwo, counttwoс, flagtwo);
+            }
+            else
+            {
+                mass[1] = new TwoDimensions( counttwo, counttwoс);
+            }
+
+            Console.WriteLine("Как вы хотите заполянть массив ступенчатых");
+            bool flagst = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Каок кол-во строк вы хотите в ступенчатом масииве");
+            int countst = int.Parse(Console.ReadLine());
+
+            if (flagst)
+            {
+                mass[2] = new StepDimensions( countst, flagst);
+            }
+            else
+            {
+                mass[2] = new StepDimensions( countst);
+            }
+            
+            for(int i=0; i<mass.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("Весь массив одномерных");
+                    Console.WriteLine("Среднее значение одномерных");
+                }
+                mass[i].Print();
+                Console.WriteLine(mass[i].Mid());
+            }
+        }
+    }
+}
---
+++ TwoDimensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_2
+{
+    sealed class TwoDimensions : BaseClass
+    {
+        private int[,] array;
+        
+        static private Random rnd = new Random();
+        public TwoDimensions(int RowCount, int CountColumn, bool Flag=false): base(Flag)
+        {
+            array = new int[RowCount,CountColumn];
+
+            base.TypeOfInizialization();
+
+        }
+
+        protected override void UserFill()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j=0; j < array.GetLength(1);j++)
+                {
+                    Console.WriteLine("Введите элемент");
+                    array[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+
+        protected override void RndFill()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1);)
+                {
+                    array[i,j] = rnd.Next(-100, 101);
+                }
+            }
+        }
+
+        public override int Mid()
+        {
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1);j++)
+                {
+                    sum += array[i,j];
+                }
+            }
+            Console.WriteLine("Среднее значение двумерных");
+            return sum / (array.Length);
+            
+        }
+
+        public void Recreate(int ColumnCount, int RowCount, bool Flag)
+        {
+            array = new int[RowCount,ColumnCount];
+            base.flag = Flag;
+            base.TypeOfInizialization();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("весь массив двумерных");
+            for (int i=0; i < array.GetLength(0); i++)
+            {
+                string final = "";
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    final += array[i, j].ToString()+" ";
+                }
+                Console.WriteLine(final);
+            }
+
+        }
+    }
+}
